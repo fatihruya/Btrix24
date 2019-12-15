7031da877d42a228d0c0d72856581590d77921f5
fatihruya <57195656+fatihruya@users.noreply.github.com>
2019-12-15 23:50:48 +0000

    updated task class

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,6 +1,13 @@
 package tests;
 
 public class task {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("Hello World");
+    //this line is added from GitHub, see you in Intellij
+
+    }
 
 
 }
