c564d618618980ee88c5f85a24439f9e6d93de21
fatihruya <57195656+fatihruya@users.noreply.github.com>
2019-12-16 13:21:28 +0000

    update firststudy in github

--- src/test/java/tests/firststudy.java
+++ src/test/java/tests/firststudy.java
@@ -5,5 +5,12 @@ public class firststudy {
     public static void main(String[] args) {
 
         System.out.println("in intellij");
+        
+        number(5);
+        System.out.println(number);
+    }
+    
+    public int number(int x){
+        return x;
     }
 }
