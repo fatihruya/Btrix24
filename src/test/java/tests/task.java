6ee442b30799e6877ad1bd9edab093f155f35077
Fatih <semihcanli@Gmail.com>
2019-12-16 12:41:13 +0000

    add void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -7,12 +7,8 @@ public class task {
     System.out.println("Hello World");
     //this line is added from GitHub, see you in Intellij
     }
-public void  hi(){
+    public void  hi(){
     System.out.println("Hi from Intellij");
-
-
-
-
-}
+    }
 
 }
