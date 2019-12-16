9fee57284aa7eb73764f4dec89f4b08ace2a4220
Fatih <semihcanli@Gmail.com>
2019-12-16 13:41:10 +0000

    updated code

--- src/test/java/tests/firststudy.java
+++ src/test/java/tests/firststudy.java
@@ -5,12 +5,24 @@ public class firststudy {
     public static void main(String[] args) {
 
         System.out.println("in intellij");
-        
-        number(5);
-        System.out.println(number);
+
+//        int number(5);
+//        System.out.println(number());
+
+        int result = computeSize(10, 3);
+        System.out.println(result);
     }
-    
-    public int number(int x){
-        return x;
+
+    static int computeSize(int height, int width) {
+        // Return an expression based on two arguments (variables).
+        return height * width;
     }
 }
+    
+//    public int number(int x){
+//        return x;
+//    }
+
+
+
+
